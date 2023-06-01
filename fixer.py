38e0c2774ebdeca80453a69509bf9140f3a979d2
Dylan <135184758+TeddysMoo@users.noreply.github.com>
2023-05-31 18:51:25 -0700

    Update and rename reset.py to fixer.py

--- fixer.py
+++ fixer.py
@@ -1,8 +1,11 @@
+# Written by Dylan Tuminelli using ChatGPT
+# Fixer! - Changes every file in a selected folder to the desired file type. 
+# Version 1.0 - 5/31/2023
 import os
 
 def main():
-    filetype = input("Enter the filetype: ")
-    folder_path = input("Enter the folder path: ")
+    filetype = input("Enter the desired filetype: ")
+    folder_path = input("Enter your folder's path: ")
 
     for filename in os.listdir(folder_path):
         
